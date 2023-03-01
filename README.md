83a92ec5085f25b4e252ce7964fe528c5e5fd2fa
Raghvender <raghvender1205@gmail.com>
2023-03-02 03:11:13 +0530

    Gradio App Working

--- README.md
+++ README.md
@@ -7,7 +7,14 @@ A Web based implementation of Pothole Detection using Deep Learning
 3. Add more good videos for testing
 4. Add more options and features
 
+## Usage
+`Gradio` Demo is working currently working 
+```bash
+python gradioDemo.py
+```
+
 ### Prerequisites
 1. torch 
 2. ultralytics
 3. streamlit
+4. gradio
---
+++ gradioDemo.py
@@ -0,0 +1,46 @@
+import gradio as gr
+import cv2
+import requests
+import os
+from ultralytics import YOLO
+
+model = YOLO('best.pt')
+# video_path = 'Deployment\\test_videos\\test2.mp4'
+def show_preds(video_path):
+    cap = cv2.VideoCapture(video_path)
+    while True:
+        ret, frame = cap.read()
+        if ret:
+            frame_copy = frame.copy()
+            outputs = model.predict(source=frame)
+            res = outputs[0].cpu().numpy()
+            for i, det in enumerate(res.boxes.xyxy):
+                cv2.rectangle(
+                    frame_copy, 
+                    (int(det[0]), int(det[1])),
+                    (int(det[2]), int(det[3])),
+                    color=(0, 0, 255),
+                    thickness=2,
+                    lineType=cv2.LINE_AA
+                )
+            yield cv2.cvtColor(frame_copy, cv2.COLOR_BGR2RGB)
+
+input_video = [
+    gr.components.Video(type='filepath', label='Input Video'),
+]
+outputs_video = [
+    gr.components.Image(type='numpy', label='Output Image'),
+]
+interface_video = gr.Interface(
+    fn=show_preds,
+    inputs=input_video,
+    outputs=outputs_video,
+    title='Pothole Detection',
+    #examples=video_path,
+    cache_examples=False,
+)
+
+gr.TabbedInterface(   
+    [interface_video],
+    tab_names=['Video Inference']
+).queue().launch()
